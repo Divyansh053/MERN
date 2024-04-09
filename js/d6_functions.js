08897f3cd0534274b5b7ab80b70012b91b26a114
Divyansh053 <142472427+Divyansh053@users.noreply.github.com>
2024-04-09 16:01:19 +0530

    day6

--- js/d6_functions.js
+++ js/d6_functions.js
@@ -64,30 +64,39 @@
 
 
 
-//to select multiple case sensitives
-const arr2=[
-    'Delhi, India',
-    'Mumbai, india',
-    'Washington, USA',
-    'New York, USA',
-    'Beijing, China',
-    'Wuhan, China',
-];
+// //to select multiple case sensitives
+// const arr2=[
+//     'Delhi, India',
+//     'Mumbai, india',
+//     'Washington, USA',
+//     'New York, USA',
+//     'Beijing, China',
+//     'Wuhan, China',
+// ];
 
-const ans2=arr2.filter((s)=>s.toLowerCase().includes('india'));
-    // const ns = s.toLowerCase();
-    // // if(ns.includes('india')){
-//     if(s.toLowerCase().includes('india')){
-//        return true;
-//     }
-//     else return false;
-//    })
-   console.log(ans2);
-   const ans3= ans2.map((a)=>a.toUpperCase());
-   console.log(ans3);
+// const ans2=arr2.filter((s)=>s.toLowerCase().includes('india'));
+//     // const ns = s.toLowerCase();
+//     // // if(ns.includes('india')){
+// //     if(s.toLowerCase().includes('india')){
+// //        return true;
+// //     }
+// //     else return false;
+// //    })
+//    console.log(ans2);
+//    const ans3= ans2.map((a)=>a.toUpperCase());
+//    console.log(ans3);
 
-   const arr3= [10,22,32,50];
-   const ans4=  arr3.reduce((a,b,c,d)=>{
-console.log(a,b,c,d);
-return a+b;
+//    const arr3= [10,22,32,50];
+//    const ans4=  arr3.reduce((a,b,c,d)=>{
+// console.log(a,b,c,d);
+// return a+b;
+//    });
+//    console.log(ans4);
+
+
+
+   const arr= ['ab','cd','ef','gh'];
+   const ans=  arr.reduce((total,curr)=>{
+console.log(total,curr);
    });
+   console.log(ans);
---
+++ nodeJS/d1_module.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+    console.log(a+b);
+}
+function mul(a,b){
+    console.log(a*b);
+}
+
+module.exports = {
+ fn1: sum,
+ fn2: mul,
+};
